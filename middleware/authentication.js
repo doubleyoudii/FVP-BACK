1551813d40ff9c61ed52dad56572c8a35e8ce196
unknown <willyumburjer@gmail.com>
2019-12-11 11:40:44 +0800

    Disabled auth

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,27 +3,28 @@ const jwt = require("jsonwebtoken");
 const authenticate = (req, res, next) => {
   //Do something in Token
 
-  const bearerHeader = req.header("authorization");
-  if (bearerHeader !== undefined) {
-    const bearer = bearerHeader.split(" ");
-    const bearerToken = bearer[1];
-    req.body.token = bearerToken;
+  // const bearerHeader = req.header("authorization");
+  // if (bearerHeader !== undefined) {
+  //   const bearer = bearerHeader.split(" ");
+  //   const bearerToken = bearer[1];
+  //   req.body.token = bearerToken;
 
-    jwt.verify(req.body.token, process.env.JWT_SALT, async (err, authData) => {
-      if (err) {
-        res.status(403).json({
-          message: "Forbidden"
-        });
-      } else {
-        req.body.user = authData;
-        next();
-      }
-    });
-  } else {
-    res.status(403).json({
-      message: "Forbidden "
-    });
-  }
+  //   jwt.verify(req.body.token, process.env.JWT_SALT, async (err, authData) => {
+  //     if (err) {
+  //       res.status(403).json({
+  //         message: "Forbidden"
+  //       });
+  //     } else {
+  //       req.body.user = authData;
+  //       next();
+  //     }
+  //   });
+  // } else {
+  //   res.status(403).json({
+  //     message: "Forbidden "
+  //   });
+  // }
+  next();
 };
 
 module.exports = { authenticate };
